20dacea5f69084bada3c997642039eb87d311d98
reinier303 <30725987+reinier303@users.noreply.github.com>
2019-12-13 18:52:28 +0100

    Added weapon system start

---
+++ Assets/Scripts/Upgrades2.0/BulletWeapon.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletWeapon : Weapon
+{
+    PlayerStats playerStats;
+    ObjectPooler objectPooler;
+    AudioManager audioManager;
+    GameManager gameManager;
+    private MonoBehaviour player;
+
+    float FireRate;
+    private bool FireCooldown;
+    public float extraRotation = 1f;
+    int projectileCount;
+    bool multipleProjectiles;
+    public ParticleSystem Muzzle;
+
+
+    private void Start()
+    {
+        objectPooler = ObjectPooler.Instance;
+        playerStats = PlayerStats.Instance;
+        audioManager = AudioManager.Instance;
+        gameManager = GameManager.Instance;
+        FireRate = playerStats.FireRate;
+        multipleProjectiles = playerStats.MultipleProjectiles;
+        projectileCount = (int)playerStats.ProjectileCount;
+        player = gameManager.Player.GetComponent<PlayerFire>(); ;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && !gameManager.gamePaused)
+        {
+            Fire();
+        }
+    }
+
+    public override void Fire()
+    {
+        if (!FireCooldown)
+        {
+            Muzzle.Play();
+            audioManager.Play("Shot");
+            player.StartCoroutine(StartFireCooldown());
+
+            int direction = 1;
+            for (int i = 0; i < projectileCount; i++)
+            {
+                objectPooler.SpawnFromPool("Bullet", player.transform.position + (player.transform.up / 3), Quaternion.Euler(player.transform.eulerAngles.x, player.transform.eulerAngles.y, player.transform.eulerAngles.z + (i * extraRotation * direction)));
+                direction *= -1;
+            }
+
+        }
+    }
+
+    IEnumerator StartFireCooldown()
+    {
+        FireCooldown = true;
+        yield return new WaitForSeconds(1 / FireRate);
+        FireCooldown = false;
+    }
+}
---
+++ Assets/Scripts/Upgrades2.0/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private List<Module> modules;
+
+    public virtual void Fire()
+    {
+        //This method is meant to be overriden
+    }
+}
+
+public struct Module
+{
+    string stat;
+    bool unlocked;
+    int cap;
+}
